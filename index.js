1c9d6ab5fde9c29ba26ccaca07da88a53259059d
khalid jamal <kh.jamal95@gmail.com>
2018-09-04 05:32:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function createPost() {
-  // create template functions
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
